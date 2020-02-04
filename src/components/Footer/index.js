638e4a18d0a5d6fab814c1fa5d09b8d45fd74179
phgrund <phrgrund@gmail.com>
2020-02-04 18:33:06 -0300

    curriculum added for download

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,6 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <p>Made with <span role="img" aria-label="heart emoji">&#10084;&#65039;</span> by phgrund</p>
+      <p>Download my curriculum vitae <a href="https://1drv.ms/w/s!AlmrayiMNXoxgYBk7a_2cK5uGo3I8Q?e=ZnxFOw"  target="_blank" rel="noopener noreferrer">here</a></p>
     </footer>
   );
 }
